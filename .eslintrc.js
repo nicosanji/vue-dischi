c7dbc8dcd5bf1fde13c74de280784d1995853a3d
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-15 14:49:09 +0100

    START: vue-dischi

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
+  ignorePatterns: ["dist/*"],
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Titolo pagina</h1>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
